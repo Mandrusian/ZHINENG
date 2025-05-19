adb4de8721afcae35f8ce61fa024fa77c8befb1b
Xavi <Xavi>
2025-05-20 09:44:20 +1000

    Describe your changes here

--- script.js
+++ script.js
@@ -62,8 +62,8 @@ document.addEventListener('DOMContentLoaded', () => {
         contentArea.innerHTML = `<iframe src="${url}" class="browser-iframe"></iframe>`;
     }
 
-    // On page load, embed Qwant by default
-    embedSite('https://www.qwant.com/?l=en');
+    // On page load, embed Google Custom Search by default
+    embedSite('https://cse.google.com/cse?cx=c18f3250b0b5c45f0');
 
     // Suggestion click
     document.querySelectorAll('.suggestion').forEach(suggestion => {
